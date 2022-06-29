4a22510fed374023b7a4e3b2d2b4eafc76071f6a
SimonZech <simon.zech@fau.de>
2022-06-29 13:19:30 +0200

    Fixed typos

--- matlab_example.m
+++ matlab_example.m
@@ -61,7 +61,7 @@ jump_parameter = 1;
 output_rate    = 1;
 
 
-outputData = run_cam(nx, ny, numSteps,porosity,jump_param,output_rate);
+outputData = run_cam(nx, ny, num_steps,porosity,jump_parameter,output_rate);
 
 if ~exist('output', 'dir')
     mkdir output;
@@ -69,7 +69,7 @@ end
 
 numSolidPixels = sum(outputData(:,1)>0);
 
-for i = 1 : numSteps + 1
+for i = 1 : num_steps + 1
     if sum(outputData(:,i)>0) ~= numSolidPixels
         error('Error. Number of solid pixels changed.')
     end
@@ -79,16 +79,18 @@ end
 
 
 function visualizeBinaryMatrix(inputMatrix, figPath) 
-    inputMatrix = inputMatrix > 0;
-    [row,col] = find(inputMatrix > 0);
-    outputPixels = [col-1 size(inputMatrix,2)-row];
-    axh = axes('XTick',0:1:10,'YTick',0:1:10); % Assumes square axes
-    grid on
-    axis equal
-    xlim([0,10])
-    ylim([0,10])
-    hold on
-    % Plot rectangles 
-    rh = arrayfun(@(i) rectangle('position',[outputPixels(i,:), 1, 1], 'FaceColor','k','EdgeColor', 'none'), 1:size(outputPixels,1)); 
-    exportgraphics(axh,figPath)
+%     inputMatrix = inputMatrix > 0;
+%     [row,col] = find(inputMatrix > 0);
+%     outputPixels = [col-1 size(inputMatrix,2)-row];
+%     axh = axes('XTick',0:1:10,'YTick',0:1:10); % Assumes square axes
+%     grid on
+%     axis equal
+%     xlim([0,10])
+%     ylim([0,10])
+%     hold on
+%     % Plot rectangles 
+%     rh = arrayfun(@(i) rectangle('position',[outputPixels(i,:), 1, 1], 'FaceColor','k','EdgeColor', 'none'), 1:size(outputPixels,1)); 
+%     exportgraphics(axh,figPath)
+    inputMatrix = repelem(inputMatrix, floor(1000/size(inputMatrix,1)), floor(1000/size(inputMatrix,2)));
+    imwrite(~inputMatrix, figPath);
 end
