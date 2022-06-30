49381bea563102b78e1e321bbc0795f91834ae81
SimonZech <simon.zech@fau.de>
2022-06-30 14:38:28 +0200

    Documentation for matlab_example.m

--- matlab_example.m
+++ matlab_example.m
@@ -1,5 +1,5 @@
-% TODO: Joona and Simon!
-
+% Brief example illustration how to run the cellular automaton, which is
+% implemented in C++, in MATLAB.
 %  ========================================================================
 %> @file  matlab_example.m
 %>
@@ -10,35 +10,15 @@
 %> @brief Brief example illustration how to run the cellular automaton,
 %         which is implemented in C++, in MATLAB.
 %>
-%> TODO: Joona and Simon: Fill this.
+%> It allows to set the size of the domain, the porosity, the
+%> jump_parameter, indicating how far cells are allowed to jump, and the
+%> number of steps the cellular automaton should run. It creates the
+%> resulting data and images visualizing the results.
 %> 
-%> The main loop repeatedly executes four steps until the parameter
-%> <code>problemData.isFinished</code> becomes <code>true</code>.
-%> These four steps are:
-%>
-%>  1. preprocessStep()
-%>  2. solveStep()
-%>  3. postprocessStep()
-%>  4. outputStep()
-%> 
-%> This routine is executed second in each loop iteration.
-%> It assembles the global system and solves it using the backslash-operator.
-%>
-%> @param  problemData  A struct with problem parameters, precomputed
-%>                      fields, and solution data structures (either filled
-%>                      with initial data or the solution from the previous
-%>                      loop iteration), as provided by configureProblem()  
-%>                      and preprocessProblem(). @f$[\text{struct}]@f$
-%> @param  nStep        The current iteration number of the main loop. 
-%>
-%> @retval problemData  The input struct enriched with solution data at
-%>                      the next step. @f$[\text{struct}]@f$
 %>
 %> This file is part of the GitHub repository
 %>   https://github.com/AndreasRupp/cellular-automaton
 %> Copyright and license conditions can be found there.
-
-
 
 %% First step is including the directory 'mex/CAM' to the MATLAB path.
 %  This allows to run all MATLAB functions of CAM. These MATLAB functions
@@ -50,48 +30,81 @@ addpath('mex/CAM')
 %  This example illustrates how to run the run_cam function, see the file
 %  mex/CAM/run_cam.m. It runs a celular automaton method on a grid of size
 %  'nx * ny' for 'num_steps' steps. Moreover, we set the porosity (i.e.the
-%  percentage of void space---not occupied by solid), the jump parameter
-%  (telling how far individual cells are allowd to jump), and an output
-%  rate (saying after how many steps an output shall be returned).
-nx             = 10;
-ny             = 10;
+%  percentage of void space---not occupied by solid) and the jump parameter
+%  (telling how far individual cells are allowd to jump) as mandatory
+%  parameters.
+nx             = 100;
+ny             = 100;
 num_steps      = 50;
 porosity       = 0.9;
 jump_parameter = 1;
+
+%  Optionally, we can set a frame rate (saying after how many steps an
+%  output shalle be returned), a boolean parameter indicating if the output
+%  of the results shall be returned or not, a boolean parameter indicating
+%  if the calculated measures shall be returned or not and a boolean
+%  parameter indicating if the number of the random seed shall be printed
+%  or not. The parameter num_random_seed allows to choose between a random
+%  seed, depending on the clock time (if set to 0), or a given seed.
 frame_rate     = 1;
+output_results = true;
+output_measures = true;
+output_random_seed = true;
+num_random_seed = 0;
 
-[outputData, measures] = run_cam(nx, ny, num_steps,porosity,jump_parameter,output_rate=frame_rate);
+%% Call the run_cam function and save the output data.
+%  This calls the run_cam function with the chosen parameters. The output
+%  consists of the matrix domain_data with nx*ny rows and a column for
+%  every step with output. The entries of domain_data are either 0,
+%  corresponding to void cells, i.e. non-solid cells, or positive integers
+%  corresponding to solid cells. The matrix measures contains 6 rows and a
+%  column for evry step with output. Every row corresponds to a certain
+%  geometric measure of the domain_data at the given step: 
+%  - row 1: Number of single solid pixels without solid neighbours.
+%  - row 2: Number of solid particles, including single solid pixels and
+%  agglomorates of solid pixels.
+%  - row 3: Total number of solid pixels. This is constant over the run of
+%  one simulation.
+%  - row 4: Total solid surface, i.e. the sum over all solid pixels of
+%  neighboring void pixels.
+%  - row 5: Mean particle size, i.e. the total number of solid pixels
+%  divided by the number of solid particles.
+%  - row 6: Number of connected fluid, i.e. void, components.
+[domain_data, measures] = run_cam(nx, ny, num_steps,porosity,...
+    jump_parameter,output_rate=frame_rate, print_results=output_results,...
+    print_measures=output_measures,print_random_seed=output_random_seed,...
+    random_seed=num_random_seed);
 
+%  This creates an output directory, if it does not exist yet. In the
+%  output directory, the images of the domain are stored.
 if not(isfolder('output'))
     mkdir('output')
-end  % not is folder
+end  
 
-num_solid_pixels = sum(outputData(:,1)>0);
-
+%  For every step with output data, we create an image file visualizing the
+%  domain with solid pixels in black and void pixels in white. The
+%  images are saved as output/fig.i.png. 
 for i = 1 : num_steps + 1
-    if (frame_rate ~= 0 && mod(i , frame_rate) == 0)
-        if sum(outputData(:,floor(i / frame_rate))>0) ~= num_solid_pixels
-            error('Error. Number of solid pixels changed.')
-        end
-        outputPic = reshape(outputData(:,floor(i / frame_rate)), [nx ny]);
-        visualizeBinaryMatrix(outputPic, strcat(strcat('output/fig.',num2str(floor(i / frame_rate) -1)),'.png')) 
+    if (frame_rate ~= 0 && mod(i , frame_rate) == 0) 
+        %  The domain vector at the given step is reshaped to a matrix of
+        %  size [nx ny].
+        domain_geo = reshape(domain_data(:,floor(i / frame_rate)),[nx ny]);
+        visualize_binary_matrix(domain_geo, strcat(strcat('output/fig.',...
+            num2str(floor(i / frame_rate) -1)),'.png')) 
     end
 end
 
-
-function visualizeBinaryMatrix(inputMatrix, figPath) 
-%     inputMatrix = inputMatrix > 0;
-%     [row,col] = find(inputMatrix > 0);
-%     outputPixels = [col-1 size(inputMatrix,2)-row];
-%     axh = axes('XTick',0:1:10,'YTick',0:1:10); % Assumes square axes
-%     grid on
-%     axis equal
-%     xlim([0,10])
-%     ylim([0,10])
-%     hold on
-%     % Plot rectangles 
-%     rh = arrayfun(@(i) rectangle('position',[outputPixels(i,:), 1, 1], 'FaceColor','k','EdgeColor', 'none'), 1:size(outputPixels,1)); 
-%     exportgraphics(axh,figPath)
-    inputMatrix = repelem(inputMatrix, floor(1000/size(inputMatrix,1)), floor(1000/size(inputMatrix,2)));
-    imwrite(~inputMatrix, figPath);
+%% This routine visualizes the given matrix by a black and white image.
+% @param geo       A matrix with entries of 0, corresponding to void pixels
+%                  and non-zero integers, corresponding to solid pixels.
+% @param fig_path  The name of the file where the image is written to.
+% The function resizes the matrix geo, s.t. the created image is at least
+% of size 500x500 pixels. In the creates image, black pixels visualize the
+% non-zero entries of geo, while white pixels correspond to the zero
+% entries of geo.
+function visualize_binary_matrix(geo, fig_path) 
+    resize_factor = max(1, min(floor(1000/size(geo,1)), ...
+        floor(1000/size(geo,2))));
+    geo = repelem(geo, resize_factor, resize_factor);
+    imwrite(~geo, fig_path);
 end
