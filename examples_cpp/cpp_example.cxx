d9091419d75870e9ecc9d12ec7006dba51644be3
AndreasRupp <andreas.rupp@fau.de>
2022-07-07 15:25:11 +0300

    Format.

--- examples_cpp/cpp_example.cxx
+++ examples_cpp/cpp_example.cxx
@@ -4,84 +4,68 @@
 
 #include <CAM/cellular_automaton.hxx>
 
-template <std::size_t size>
 /*!*************************************************************************************************
  * \brief   Prints matrix of particles.
  *
  * \param   particles      Particle number and size
  **************************************************************************************************/
-// void print_array(const std::array<unsigned int, size> particles)
-// {
-//   const unsigned int len_numbers = std::log10(size - 1) + 1;
-//   const unsigned int nx = std::sqrt(size);
-//   for (unsigned int y = 0; y < nx; ++y)
-//   {
-//     for (unsigned int x = 0; x < nx; ++x)
-//       if (particles[y * nx + x] == 0)
-//         std::cout << std::setw(len_numbers) << std::setfill('0') << particles[y * nx + x] << "  ";
-//       else
-//         std::cout << "\033[0;31m" << std::setw(len_numbers) << std::setfill('0')
-//                   << particles[y * nx + x] << "\033[0m"
-//                   << "  ";
-//     std::cout << std::endl;
-//   }
-// }
-void print_array(const std::array<unsigned int, size> particles, std::array<unsigned int, 2> nx)
+template <std::size_t size_p, std::size_t dim>
+void print_array(const std::array<unsigned int, size_p> particles, std::array<unsigned int, dim> nx)
 {
-	const unsigned int len_numbers = std::log10(size - 1) + 1;
-	const unsigned int dim = nx.size();
-	const unsigned int dim_2_size = nx[0] * nx[1];
-	const unsigned int n = std::sqrt(dim_2_size);
-	if (dim == 3)
-	{
-		for (unsigned int z = 0; z < nx[2]; ++z)
-		{
+  const unsigned int len_numbers = std::log10(size_p - 1) + 1;
+  unsigned int index;
+  if (dim == 3)
+  {
+    for (unsigned int z = 0; z < nx[2]; ++z)
+    {
       std::cout << "z = " << z << std::endl;
-			for (unsigned int y = 0; y < nx[1]; ++y)
-  		{
-				for (unsigned int x = 0; x < nx[0]; ++x)
-				{
-			    if (particles[y * n + x] == 0)
-			    {
-						std::cout << std::setw(len_numbers) << std::setfill('0') << particles[y * n + x] << "  ";
-					}
-					else
-					{
-				    std::cout << "\033[0;31m" << std::setw(len_numbers) << std::setfill('0')
-				              << particles[y * n + x] << "\033[0m"
-			        	      << "  ";
-					}
-  			}
+      for (unsigned int y = 0; y < nx[1]; ++y)
+      {
+        for (unsigned int x = 0; x < nx[0]; ++x)
+        {
+          index = (z * nx[1] + y) * nx[0] + x;
+          if (particles[index] == 0)
+          {
+            std::cout << std::setw(len_numbers) << std::setfill('0') << particles[index] << "  ";
+          }
+          else
+          {
+            std::cout << "\033[0;31m" << std::setw(len_numbers) << std::setfill('0')
+                      << particles[index] << "\033[0m"
+                      << "  ";
+          }
+        }
         std::cout << std::endl;
-			}
+      }
       std::cout << std::endl;
-		}
-	}
-	else if (dim == 2)
-	{	
-		for (unsigned int y = 0; y < nx[1]; ++y)
-  	{
-			for (unsigned int x = 0; x < nx[0]; ++x)
-			{
-		    if (particles[y * n + x] == 0)
-		  	{
-					std::cout << std::setw(len_numbers) << std::setfill('0') << particles[y * n + x] << "  ";
-				}
-				else
-				{
-			    std::cout << "\033[0;31m" << std::setw(len_numbers) << std::setfill('0')
-			              << particles[y * n + x] << "\033[0m"
-			              << "  ";
-				}
-  		}
+    }
+  }
+  else if (dim == 2)
+  {
+    for (unsigned int y = 0; y < nx[1]; ++y)
+    {
+      for (unsigned int x = 0; x < nx[0]; ++x)
+      {
+        if (particles[y * nx[0] + x] == 0)
+        {
+          std::cout << std::setw(len_numbers) << std::setfill('0') << particles[y * nx[0] + x]
+                    << "  ";
+        }
+        else
+        {
+          std::cout << "\033[0;31m" << std::setw(len_numbers) << std::setfill('0')
+                    << particles[y * nx[0] + x] << "\033[0m"
+                    << "  ";
+        }
+      }
       std::cout << std::endl;
-		}
-	}
-	else
-	{	
-		std::cout << "Printing works only for 2D and 3D." << std::endl;
-		return;
-	}
+    }
+  }
+  else
+  {
+    std::cout << "Printing works only for 2D and 3D." << std::endl;
+    return;
+  }
 }
 /*!*************************************************************************************************
  * \brief   Main function.
@@ -97,12 +81,12 @@ void print_array(const std::array<unsigned int, size> particles, std::array<unsi
  **************************************************************************************************/
 int main()
 {
-  constexpr std::array<unsigned int, 2> nx = {10, 10};
+  constexpr std::array<unsigned int, 3> nx = {10, 10, 10};
   const unsigned int n_moves = 5;
   const double porosity = 0.9;
   const double jump_param = 1.;
   cellular_automaton<nx> domain(porosity, jump_param);
-  std::cout<< "Seed: " << domain.random_seed() <<std::endl;
+  std::cout << "Seed: " << domain.random_seed() << std::endl;
 
   print_array(domain.fields(), nx);
 
--- include/CAM/cellular_automaton.hxx
+++ include/CAM/cellular_automaton.hxx
@@ -80,8 +80,9 @@ class cellular_automaton
     unsigned int new_pos = 0;
     for (unsigned int i = 0; i < dim; ++i)
     {
-      coord = (position / direct_neigh_[2 * i + 1] + move / (int)direct_neigh_[2 * i + 1] + n_fields_) %
-              nx[i];
+      coord =
+        (position / direct_neigh_[2 * i + 1] + move / (int)direct_neigh_[2 * i + 1] + n_fields_) %
+        nx[i];
       new_pos += coord * direct_neigh_[2 * i + 1];
     }
     // const unsigned int x_coord = (position % nx + (nx * ny + move) % nx) % nx;
