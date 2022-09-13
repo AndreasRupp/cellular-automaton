02a255628064279e266bdee10e8b5935a925ff9f
AndreasRupp <andreas.rupp@fau.de>
2022-09-13 21:01:55 +0300

    Print way faster.

--- matlab_functions/print.m
+++ matlab_functions/print.m
@@ -41,7 +41,7 @@ function print(nx, data, rate, steps)
             case 1
                 i = mod(i + 1, steps);
             case 2
-                i = mod(i + 1, steps);
+                i = mod(i - 1, steps);
             otherwise
                 return
         end  % switch event
@@ -70,19 +70,24 @@ function print_update(nx, data)
     if dim == 3
         z = 0:nx(3);
     end
+    if dim == 1
+        y = [0 1];
+        nx(2) = 1;
+        dim = 2;
+    end
     cla
 
     if dim == 3
         % Creates gridlines for the domain
         [X1, Y1, Z1] = meshgrid(x,y,z);
-        X1 = permute(X1,[2 1 3]); Y1 = permute(Y1,[2 1 3]); Z1 = ...
-            permute(Z1,[2 1 3]);
+        X1 = permute(X1,[2 1 3]); Y1 = permute(Y1,[2 1 3]);
+        Z1 = permute(Z1,[2 1 3]);
         X1(end+1,:,:) = NaN; Y1(end+1,:,:) = NaN; Z1(end+1,:,:) = NaN;
-        [X2, Y2, Z2] = meshgrid(x,y([1 end]),z);
+        [X2, Y2, Z2] = meshgrid(x,y,z);
         X2(end+1,:,:) = NaN; Y2(end+1,:,:) = NaN; Z2(end+1,:,:) = NaN;
-        [X3, Y3, Z3] = meshgrid(x,y,z([1 end]));
-        X3 = permute(X3,[3 1 2]); Y3 = permute(Y3,[3 1 2]); Z3 = ...
-            permute(Z3,[3 1 2]);
+        [X3, Y3, Z3] = meshgrid(x,y,z);
+        X3 = permute(X3,[3 1 2]); Y3 = permute(Y3,[3 1 2]);
+        Z3 = permute(Z3,[3 1 2]);
         X3(end+1,:,:) = NaN; Y3(end+1,:,:) = NaN; Z3(end+1,:,:) = NaN;
 
         h = line([X1(:);X2(:);X3(:)], [Y1(:);Y2(:);Y3(:)], ...
@@ -90,86 +95,47 @@ function print_update(nx, data)
         set(h, 'Color',[0.5 0.5 1], 'LineWidth',1, 'LineStyle','-');
 
         % Creates faces of the cube (particle)
-        for i = 1:size(data)
-            if data(i) ~= 0
-                xi = mod((i-1),nx(1));
-                yi = mod(floor((i-1)/nx(1)),nx(2));
-                zi = mod(floor((i-1)/(nx(1)*nx(2))),nx(3));
-                cube_xy = [0 1 1 0 0;
-                           0 0 1 1 0;
-                           0 0 0 0 0];
-                patch('XData', xi + cube_xy(1,:), 'YData', yi + ...
-                    cube_xy(2,:), 'ZData', zi + cube_xy(3,:))
-                patch('XData', xi + cube_xy(1,:), 'YData', yi + ...
-                    cube_xy(2,:), 'ZData', zi + cube_xy(3,:) + 1)
+        x = repmat(mod(0:size(data)-1,nx(1))', 5);
+        x = x((data ~= 0), :).';
+        y = repmat(mod(floor((0:size(data)-1)./nx(1)),nx(2))', 5);
+        y = y((data ~= 0), :).';
+        z = repmat(mod(floor((0:size(data)-1)./(nx(1)*nx(2))),nx(3))', 5);
+        z = z((data ~= 0), :).';
 
-                cube_xz = [0 1 1 0 0;
-                           0 0 0 0 0;
-                           0 0 1 1 0];
-                patch('XData', xi + cube_xz(1,:), 'YData', yi + ...
-                    cube_xz(2,:), 'ZData', zi + cube_xz(3,:))
-                patch('XData', xi + cube_xz(1,:), 'YData', yi + ...
-                    cube_xz(2,:) + 1, 'ZData', zi + cube_xz(3,:))
-
-                cube_yz = [0 0 0 0 0;
-                           0 1 1 0 0;
-                           0 0 1 1 0];
-                patch('XData', xi + cube_yz(1,:), 'YData', yi + ...
-                    cube_yz(2,:), 'ZData', zi + cube_yz(3,:))
-                patch('XData', xi + cube_yz(1,:) + 1, 'YData', yi + ...
-                    cube_yz(2,:), 'ZData', zi + cube_yz(3,:))
-            end
-        end
+        cube_xy = [0 1 1 0 0; 0 0 1 1 0; 0 0 0 0 0];
+        patch('XData', x + cube_xy(1,:)', 'YData', y + cube_xy(2,:)', ...
+            'ZData', z + cube_xy(3,:)')
+        patch('XData', x + cube_xy(1,:)', 'YData', y + cube_xy(2,:)', ...
+            'ZData', z + cube_xy(3,:)' + 1)
+        cube_xz = [0 1 1 0 0; 0 0 0 0 0; 0 0 1 1 0];
+        patch('XData', x + cube_xz(1,:)', 'YData', y + cube_xz(2,:)', ...
+            'ZData', z + cube_xz(3,:)')
+        patch('XData', x + cube_xz(1,:)', 'YData', y + cube_xz(2,:)'+1, ...
+            'ZData', z + cube_xz(3,:)');
+        cube_yz = [0 0 0 0 0; 0 1 1 0 0; 0 0 1 1 0];
+        patch('XData', x + cube_yz(1,:)', 'YData', y + cube_yz(2,:)', ...
+            'ZData', z + cube_yz(3,:)')
+        patch('XData', x + cube_yz(1,:)'+1, 'YData', y + cube_yz(2,:)', ...
+            'ZData', z + cube_yz(3,:)')
     end
 
     if dim == 2
         % Creates gridlines for the domain
         [X1, Y1] = meshgrid(x,y);
-        X1 = permute(X1,[2 1 3]); Y1 = permute(Y1,[2 1 3]);
-        X1(end+1,:,:) = NaN; Y1(end+1,:,:) = NaN;
-        [X2, Y2] = meshgrid(x,y([1 end]));
-        X2(end+1,:,:) = NaN; Y2(end+1,:,:) = NaN;
-        [X3, Y3] = meshgrid(x,y);
-        X3 = permute(X3,[3 1 2]); Y3 = permute(Y3,[3 1 2]);
-        X3(end+1,:,:) = NaN; Y3(end+1,:,:) = NaN;
-
-        h = line([X1(:);X2(:);X3(:)], [Y1(:);Y2(:);Y3(:)]);
+        X1 = permute(X1,[2 1]); Y1 = permute(Y1,[2 1]);
+        X1(end+1,:) = NaN; Y1(end+1,:) = NaN;
+        [X2, Y2] = meshgrid(x,y);
+        X2(end+1,:) = NaN; Y2(end+1,:) = NaN;
+        h = line([X1(:);X2(:)], [Y1(:);Y2(:)]);
         set(h, 'Color',[0.5 0.5 1], 'LineWidth',1, 'LineStyle','-');
 
         % Creates faces of the square (particle)
         cube_xy = [0 1 1 0 0; 0 0 1 1 0];
-        x = repmat(mod(0:size(data)-1,nx(1))', 5) + cube_xy(1,:);
+        x = repmat(mod(0:size(data)-1,nx(1))', 5);
         x = x((data ~= 0), :);
-        y = repmat(mod(floor((0:size(data)-1)./nx(1)),nx(2))', 5) + ...
-            cube_xy(2,:);
+        y = repmat(mod(floor((0:size(data)-1)./nx(1)),nx(2))', 5);
         y = y((data ~= 0), :);
-        patch('XData', x', 'YData', y')
-    end
-
-    if dim == 1
-        % Creates gridlines for the domain
-        y = [0 1];
-        [X1, Y1] = meshgrid(x,y);
-        X1 = permute(X1,[2 1 3]); Y1 = permute(Y1,[2 1 3]);
-        X1(end+1,:,:) = NaN; Y1(end+1,:,:) = NaN;
-        [X2, Y2] = meshgrid(x,y([1 end]));
-        X2(end+1,:,:) = NaN; Y2(end+1,:,:) = NaN;
-        [X3, Y3] = meshgrid(x,y);
-        X3 = permute(X3,[3 1 2]); Y3 = permute(Y3,[3 1 2]);
-        X3(end+1,:,:) = NaN; Y3(end+1,:,:) = NaN;
-
-        h = line([X1(:);X2(:);X3(:)], [Y1(:);Y2(:);Y3(:)]);
-        set(h, 'Color',[0.5 0.5 1], 'LineWidth',1, 'LineStyle','-');
-
-        % Creates faces of the square (particle)
-        for i = 1:size(data)
-            if data(i) ~= 0
-                xi = mod((i-1),nx(1));
-                cube_xy = [0 1 1 0 0;
-                           0 0 1 1 0];
-                patch('XData', xi + cube_xy(1,:), 'YData', cube_xy(2,:))
-            end
-        end
+        patch('XData', (x + cube_xy(1,:))', 'YData', (y + cube_xy(2,:))')
     end
 
     axis equal
