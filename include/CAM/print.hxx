26856aea0db273dfa08f76f91888d2e3aeec225a
AndreasRupp <andreas.rupp@fau.de>
2022-10-25 22:26:44 +0300

    Clean up print function.

--- include/CAM/print.hxx
+++ include/CAM/print.hxx
@@ -26,15 +26,10 @@ void print_1d(const fields_array_t& particles,
   {
     index = init_index + x;
     if (particles[index] == 0)
-    {
       std::cout << std::setw(len_numbers) << std::setfill('0') << particles[index] << "  ";
-    }
     else
-    {
       std::cout << "\033[0;31m" << std::setw(len_numbers) << std::setfill('0') << particles[index]
-                << "\033[0m"
-                << "  ";
-    }
+                << "\033[0m  ";
   }
   std::cout << std::endl;
 }
@@ -66,30 +61,19 @@ void print_n_dim(const fields_array_t& particles,
         coord_i_helper = coord_i_helper / nx[i - 2];
         std::cout << i;
         if (i % 10 == 1 && i != 11)
-        {
           std::cout << "st";
-        }
         else if (i % 10 == 2 && i != 12)
-        {
           std::cout << "nd";
-        }
         else if (i % 10 == 3 && i != 13)
-        {
           std::cout << "rd";
-        }
         else
-        {
           std::cout << "th";
-        }
         std::cout << " coord: " << coord_i_helper % nx[i - 1] << "  ";
       }
       std::cout << std::endl;
     }
     for (unsigned int i = 0; i < nx[n_dim - 1]; ++i)
-    {
-      unsigned int init_index_loc = (init_index + i) * nx[n_dim - 2];
-      print_n_dim<n_dim - 1>(particles, nx, init_index_loc);
-    }
+      print_n_dim<n_dim - 1>(particles, nx, (init_index + i) * nx[n_dim - 2]);
   }
 }
 /*!*************************************************************************************************
