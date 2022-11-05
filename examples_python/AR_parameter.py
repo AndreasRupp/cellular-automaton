ec6c946b2646d6f0b164834b4f74dc466685cff8
AndreasRupp <andreas.rupp@fau.de>
2022-11-05 23:07:44 +0200

    cil_estimator becomes submodule.

--- examples_python/AR_parameter.py
+++ examples_python/AR_parameter.py
@@ -13,6 +13,9 @@ import os, sys
 # Function main.
 # --------------------------------------------------------------------------------------------------
 def main(debug_mode):
+  start_time = datetime.now()
+  print("Starting time is", start_time)
+
   try:
     import CAM
   except (ImportError, ModuleNotFoundError) as error:
@@ -22,6 +25,7 @@ def main(debug_mode):
   try:
     import cil_estimator as cil
   except (ImportError, ModuleNotFoundError) as error:
+    print("No installed cil_estimator package found! Using local cil_estimator.")
     sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../../cil_estimator.git")
     import cil_estimator as cil
 
@@ -41,7 +45,7 @@ def main(debug_mode):
     const.debug_mode      = debug_mode
 
     PyCAM = CAM.include(const)
-    CAM_wrapper = PyCAM( 0.7, jump_size )
+    CAM_wrapper = PyCAM( 0.3, jump_size )
 
     for step in range(n_steps):
       CAM_wrapper.move_particles()
@@ -49,63 +53,45 @@ def main(debug_mode):
     return CAM_wrapper.fields()
   # ------------------------------------------------------------------------------------------------
 
-  start_time = datetime.now()
-  print("Starting time is", start_time)
-
-  n_iter = 100
+  n_iter = 200
   data   = [[0] * (nx * nx)] * n_iter
   values = [0] * 10
   lower_bound = int(0.3*nx*nx)
   upper_bound = int(0.3*nx*nx)
   step        = 1
-  radii  = range(int(0.3*nx*nx),int(0.5*nx*nx),1)
-  # radii  = range(int(0.35*nx*nx),int(0.475*nx*nx),10)
+  radii  = range(int(0.3*nx*nx),int(0.55*nx*nx),1)
 
   for iter in range(n_iter):
     data[iter] = run_cam(5, nx, debug_mode)
-  func = cil.estimator( data, radii, PyCAM.bulk_distance, [20] * 5 )
-
-  import numpy as np
-
-
-  
-
-  # plotter = cil.plot_correlation_vectors(func)
-  # plotter = cil.plot_mean_vector(func)
-  func.choose_radii(10)
-
-  vec_mat = func.correlation_vector_matrix
-  derivative = [sum(vec_mat[i+1]) -  sum(vec_mat[i]) for i in range(len(vec_mat)-1)]
-  plt.plot(func.radii[:-1], derivative)
-    # plt.hist(derivative, 10, cumulative=False, density=False, facecolor='g', alpha=0.75)
-  plt.show()
-
-
-  # plotter = cil.plot_correlation_vectors(func, plotter, 'r.')
-  # plotter.show()
-
-  # for jump_size in range(10):
-  #   for iter in range(n_iter):
-  #     data[iter] = run_cam(jump_size, nx, debug_mode)
-  #   values[jump_size] = func.evaluate( data )
 
   end_time = datetime.now()
+  print("CAM data acquired at", end_time, "after", end_time-start_time)
+
+  func = cil.estimator(data, radii, PyCAM.bulk_distance, [10] * 20)
+
+  fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(18, 5))
+  ax[0] = cil.plot_correlation_vectors(func, ax[0])
+  ax[0] = cil.plot_mean_vector(func, ax[0])
+
+  func.choose_radii(10, min_value_shift=0.1, max_value_shift=-0.1)
+  
+  ax[0] = cil.plot_correlation_vectors(func, ax[0], 'r.')
+  
+  end_time = datetime.now()
+  print("Objective function setup at", end_time, "after", end_time-start_time)
+
+  for jump_size in range(len(values)):
+    for iter in range(n_iter):
+      data[iter] = run_cam(jump_size, nx, debug_mode)
+    values[jump_size] = func.evaluate( data )
+
+  
+  end_time = datetime.now()
   print("Program ended at", end_time, "after", end_time-start_time)
-
-  # print(stats.chisquare(data))
-
-  # mat_of_correlation_vec = func.correlation_matrix()
-
-  # rv = stats.chi2(mat_of_correlation_vec)
-  # plt.plot(func.radii, rv.pdf(func.radii), 'k-', lw=2, label='frozen pdf')
-  # r = chi2.rvs(func.radii)
-  # plt.hist(r, density=True, histtype='stepfilled', alpha=0.2)
-  # plt.legend(loc='best', frameon=False)
-  # plt.show()
   
 
-  # plt.plot(range(10), values, 'ro')
-  # plt.show()
+  ax[1].plot(range(len(values)), values, 'ro')
+  plt.show()
 
 
 # --------------------------------------------------------------------------------------------------
