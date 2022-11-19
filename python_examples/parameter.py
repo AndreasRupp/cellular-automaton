015d9a9f5c6afd68ae9c09b1019a7d31d836bb90
AndreasRupp <andreas.rupp@fau.de>
2022-11-20 01:41:29 +0200

    Standard bin selection.

--- python_examples/parameter.py
+++ python_examples/parameter.py
@@ -81,7 +81,7 @@ def main(debug_mode):
   ax[0,0] = ecdf.plot_mean_vector(func, ax[0,0])
   ax[1,0] = ecdf.plot_chi2_test(func, ax[1,0], 20)
 
-  func.choose_bins(n_choose_bins, min_value_shift, max_value_shift, "uniform_x")
+  func.choose_bins(n_choose_bins, min_value_shift, max_value_shift)
   
   ax[0,0] = ecdf.plot_ecdf_vectors(func, ax[0,0], 'r.')
   ax[1,1] = ecdf.plot_chi2_test(func, ax[1,1])
