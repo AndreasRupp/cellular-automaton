ed5835d5f57ef9be6554d660a09e383e9a10da78
Jobe2000 <jobe2000@gmail.com>
2022-12-05 11:14:22 +0200

    Updated plot.py

--- python_functions/plot.py
+++ python_functions/plot.py
@@ -20,20 +20,15 @@ def plot_update(axes, data, ax):
 
   if dim == 2:
     data = (data != 0)
-    cmap = colors.ListedColormap(['blue', 'red'])
-    # plt.figure(figsize=(6,6))
-    ax.pcolor(data[::-1],cmap=cmap,edgecolors='k', linewidths=3)
-    # ax.show()
+    cmap = colors.ListedColormap(['white', 'k'])
+    ax.pcolor(data[::-1],cmap=cmap,edgecolors='b', linewidths=3)
   elif dim == 3:
     data = (data != 0)
     Colors = np.empty(axes + [4], dtype=np.float32)
     # Control Transparency
     alpha = .9
-    Colors[:] = [0, 0, 1, alpha]
-    # fig = plt.figure(figsize=(6,6))
-    # ax = fig.add_subplot(111, projection='3d')
+    Colors[data] = [0, 0, 1, alpha]
     ax.voxels(data, facecolors=Colors, edgecolors='black')
-    # plt.show()
   return ax
 
 
@@ -48,17 +43,24 @@ def plot(axes, save_data, time_step):
   root.mainloop()
 
 
-def config_plot():
+def config_plot(axes):
+  dim = np.size(axes)
+  if dim == 1:
+    dim = 2
+
+  if dim == 2:    
     fig, ax = plt.subplots()
-    ax.set(xlabel='time (s)', ylabel='voltage (mV)',
-           title='Graph One')
-    return (fig, ax)
+
+  elif dim == 3:
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+  return (fig, ax)
 
 class animated_cam:
   def __init__(self, master, axes, save_data, time_step):
     self.master = master
     self.frame = Frame(self.master)
-    self.fig, self.ax = config_plot()
+    self.fig, self.ax = config_plot(axes)
     self.canvas = FigureCanvasTkAgg(self.fig, self.master)  
     self.config_window()
     self.axes      = axes
@@ -69,27 +71,25 @@ class animated_cam:
     self.frame.pack(expand=YES, fill=BOTH)
 
   def config_window(self):
-    self.canvas.mpl_connect("key_press_event", self.on_key_press)
     toolbar = NavigationToolbar2Tk(self.canvas, self.master)
     toolbar.update()
     self.canvas.get_tk_widget().pack(side=TOP, fill=BOTH, expand=1)
     self.button = Button(self.master, text="Quit", command=self._quit)
     self.button.pack(side=BOTTOM)
     self.button_next = Button(self.master, text="Next", command=self.plot_next)
-    self.button_next.pack(side=BOTTOM)
+    self.button_next.pack(side=RIGHT)
     self.button_prev = Button(self.master, text="Previous", command=self.plot_previous)
-    self.button_prev.pack(side=BOTTOM)
+    self.button_prev.pack(side=LEFT)
+    self.fig.canvas.mpl_connect('close_event', self.on_close)
 
   def draw_cam(self):
     self.ax.clear() # clear current axes
     data = self.save_data[self.time_step]
     self.ax = plot_update(self.axes, np.reshape(data, self.axes), self.ax)
-    self.ax.set(title='Graph One')
+    self.ax.set(title='Timestep: ' + str(self.time_step + 1))
+    self.ax.set_aspect('equal', 'box')
+    self.ax.axis('off')
     self.canvas.draw()
-
-  def on_key_press(event):
-    print("you pressed {}".format(event.key))
-    key_press_handler(event, self.canvas, toolbar)
 
   def _quit(self):
     self.master.quit()  # stops mainloop
@@ -101,3 +101,6 @@ class animated_cam:
   def plot_previous(self):
     self.time_step = (self.time_step - 1 + self.n_steps ) % self.n_steps
     self.draw_cam()
+
+  def on_close(self, event):
+    self.master.quit()
