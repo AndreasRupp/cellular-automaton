b24c7e21a5b3f55f70aa339443316440dc2c13fc
maxro <maximilian.roetzer@fau.de>
2022-12-05 17:12:33 +0200

    Script for different configs ecdf_parameter class more "object oriented"

---
+++ python_examples/parameter_long.py
@@ -0,0 +1,143 @@
+from __future__ import print_function
+
+import matplotlib.pyplot as plt
+import numpy as np
+from datetime import datetime
+
+import os, sys
+  
+
+# --------------------------------------------------------------------------------------------------
+# Parameter identification test.
+# --------------------------------------------------------------------------------------------------
+def parameter_identification_test(nx, porosity, n_steps, jump_parameter, n_iter, values, bins,
+  n_choose_bins, subset_sizes, min_value_shift, max_value_shift, debug_mode, name, isPlotted = 0,):
+  start_time = datetime.now()
+  print("Starting time is", start_time)
+
+  n_fields = np.prod(nx)
+  data = [[0] * n_fields] * n_iter
+
+  try:
+    import CAM
+  except (ImportError, ModuleNotFoundError) as error:
+    sys.path.append(os.path.dirname(os.path.abspath(__file__)) + os.sep  + ".." + os.sep + "import")
+    import CAM
+
+  try:
+    import ecdf_estimator as ecdf
+  except (ImportError, ModuleNotFoundError) as error:
+    print("No installed ecdf_estimator package found! Using local ecdf_estimator.")
+    # sys.path.append(os.path.dirname(os.path.abspath(__file__)) + os.sep + ".." + os.sep + ".." +
+    #   os.sep + "ecdf_estimator.git")
+    sys.path.append(os.path.dirname(os.path.abspath(__file__)) + os.sep + ".." + os.sep +
+      "submodules" + os.sep + "ecdf_estimator.git")
+    import ecdf_estimator as ecdf
+
+  const            = CAM.config()
+  const.nx         = nx
+  const.debug_mode = debug_mode
+  PyCAM            = CAM.include(const)
+
+  # ------------------------------------------------------------------------------------------------
+  def run_cam(jump_parameter, nx, porosity, n_steps, debug_mode=False):
+    const            = CAM.config()
+    const.nx         = nx
+    const.debug_mode = debug_mode
+
+    PyCAM       = CAM.include(const)
+    CAM_wrapper = PyCAM(porosity, jump_parameter)
+
+    for step in range(n_steps):
+      CAM_wrapper.move_particles()
+
+    return CAM_wrapper.fields()
+  # ------------------------------------------------------------------------------------------------
+
+  for iter in range(n_iter):
+    data[iter] = run_cam(jump_parameter, nx, porosity, n_steps, debug_mode)
+
+  end_time = datetime.now()
+  print("CAM data acquired at", end_time, "after", end_time-start_time)
+
+  func = ecdf.estimator(data, bins, PyCAM.bulk_distance, subset_sizes)
+
+  fig, ax = plt.subplots(nrows=2, ncols=2, figsize=(18, 5))
+  ax[0,0] = ecdf.plot_ecdf_vectors(func, ax[0,0])
+  ax[0,0] = ecdf.plot_mean_vector(func, ax[0,0], 'k.')
+  ax[1,0] = ecdf.plot_chi2_test(func, ax[1,0], 20)
+
+  func.choose_bins(n_choose_bins, min_value_shift, max_value_shift)
+  
+  ax[0,0] = ecdf.plot_ecdf_vectors(func, ax[0,0], 'r.')
+  ax[0,0] = ecdf.plot_mean_vector(func, ax[0,0], 'k.')
+  ax[1,1] = ecdf.plot_chi2_test(func, ax[1,1])
+  
+  end_time = datetime.now()
+  print("Objective function setup at", end_time, "after", end_time-start_time)
+
+  for jump_size in range(len(values)):
+    for iter in range(n_iter):
+      data[iter] = run_cam(jump_size, nx, porosity, n_steps, debug_mode)
+    values[jump_size] = func.evaluate( data )
+
+  end_time = datetime.now()
+  print("Program ended at", end_time, "after", end_time-start_time)
+  
+  ax[0,1].plot(range(len(values)), values, 'ro')
+
+  if not os.path.exists('output'):  os.makedirs('output')
+  plt.savefig('output/'+ name + '_parameter.png')
+  if isPlotted :
+    plt.show()
+
+
+# --------------------------------------------------------------------------------------------------
+# Define main function.
+# -------------------------------------------------------------------------------------------------- 
+if __name__ == "__main__":
+
+    test_name = 'ecdf_parameter'
+
+    try:
+        import ecdf_test
+    except (ImportError, ModuleNotFoundError) as error:
+        sys.path.append(os.path.dirname(os.path.abspath(__file__)) + os.sep  + "parameters")
+        import ecdf_test
+    used_test = ecdf_test.ecdf_parameter()
+
+    debug_mode = len(sys.argv) > 1 and sys.argv[1] == "True"
+
+
+    domain_size = [5, 10 , 25, 50, 100] 
+    sigma = [1,5,10,25,50]
+
+    
+    for i in range(len(domain_size)):
+        used_test.nx = [domain_size[i],domain_size[i]]
+        name = 'domainSize' + str(domain_size[i])
+        parameter_identification_test(used_test.nx, used_test.porosity, used_test.n_steps,
+            used_test.jump_parameter, used_test.n_iter, used_test.values, used_test.bins,
+            used_test.n_choose_bins, used_test.subset_sizes, used_test.min_value_shift,
+            used_test.max_value_shift, debug_mode, name)
+
+    used_test = ecdf_test.ecdf_parameter()
+    for i in range(len(sigma)):
+        used_test.jump_parameter = sigma[i]
+        name = 'Sigma' + str(sigma[i])
+        parameter_identification_test(used_test.nx, used_test.porosity, used_test.n_steps,
+            used_test.jump_parameter, used_test.n_iter, used_test.values, used_test.bins,
+            used_test.n_choose_bins, used_test.subset_sizes, used_test.min_value_shift,
+            used_test.max_value_shift, debug_mode, name)
+
+ 
+    used_test = ecdf_test.ecdf_parameter()
+    domain = []
+    for i in range(5):
+        domain.append(50)
+        used_test.nx = domain
+        name = 'Dim' + str(sigma[i])
+        parameter_identification_test(used_test.nx, used_test.porosity, used_test.n_steps,
+            used_test.jump_parameter, used_test.n_iter, used_test.values, used_test.bins,
+            used_test.n_choose_bins, used_test.subset_sizes, used_test.min_value_shift,
+            used_test.max_value_shift, debug_mode, name)
--- python_examples/parameters/ecdf_test.py
+++ python_examples/parameters/ecdf_test.py
@@ -1,22 +1,42 @@
 from dataclasses import dataclass
 import numpy as np
 
-
 @dataclass
-class basic_test:
+class ecdf_parameter:
   # Configure the cellular automaton method (CAM).
-  nx             = [50, 50]
-  porosity       = 0.3
+  _nx = [50, 50]
+   
+  _nf : int = np.prod(_nx)  # This is the number of fields in the CAM and can be derived from nx.
+  porosity      = 0.3
   n_steps        = 5
-  jump_parameter = 5
-
-  nf = np.prod(nx)  # This is the number of fields in the CAM and can be derived from nx.
+  _jump_parameter = 5
 
   # Configure the eCDF method.
   n_iter          = 2000                                        # Number of dataset sizes.
   values          = [0] * 10                                    # Length defines tested jump_param.
-  bins            = range(int(0.375*nf),int(0.475*nf),5)        # All radii that are checked.
+  _bins            = range(int(0.375*_nf),int(0.475*_nf),5)     # All radii that are checked.
   n_choose_bins   = 20                                          # Number of selected radii for algo.
   subset_sizes    = [40] * 50                                   # Multiplies to n_iter!
   min_value_shift = 0.1                                         # Cutoff value for small values.
-  max_value_shift = -0.1                                        # 1 + "cutoff value for large val."
+  max_value_shift = -0.1            
+  #TODO consistency: make all variables defined like that          
+  @property
+  def nx(self):
+    return self._nx
+  @property
+  def jump_parameter(self):
+    return self._jump_parameter
+  @property
+  def nf(self):
+    self._nf = np.prod(self._nx)
+    return  self._nf
+  @property
+  def bins(self):
+    self._bins = range(int(0.375*self.nf),int(0.475*self.nf),5) 
+    return self._bins
+  @nx.setter
+  def nx(self,value):
+    self._nx = value
+  @jump_parameter.setter
+  def jump_parameter(self, value):
+    self._jump_parameter = value
